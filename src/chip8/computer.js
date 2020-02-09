17d328c35f1c21d6bfda84deaf373ad66ff682e5
Davis Goff <davispgoff@gmail.com>
2020-02-09 16:31:35 -0500

    Remove timerid and add more computer functions

--- src/chip8/computer.js
+++ src/chip8/computer.js
@@ -9,29 +9,45 @@ class Computer {
 		this.chip = new Chip8(video, audio);
 		this.clockRate = 300;
 		this.stepID = null;
-		this.timerID = null;
 	}
 
+	// start the chip running at the defined clockrate
 	start() {
+		if (this.stepID || this.timerID) {
+			return;
+		}
 
+		this.stepID = window.setInterval(() => {
+			this.cpu.step();
+		}, 1000 / this.clockRate);
 	}
 
+	// stop the cpu from running
 	stop() {
 		if (this.stepID) {
 			window.clearInterval(this.stepID);
 		}
 
-		if (this.timerID) {
-			window.clearInterval(this.timerID);
-		}
-
 		this.stepID = null;
-		this.timerID = null;
 	}
 
+	// stop running and reset the cpu
 	reset() {
 		this.stop();
 		this.chip.reset();
+	}
+
+	// set the cpu input
+	setInput(input) {
+		this.cpu.setInput(input);
+	}
+
+	// load a rom
+	// Param rom = string
+	boot(rom) {
+		this.stop();
+		this.cpu.loadRom(rom);
+		this.start();
 	}
 }
 
