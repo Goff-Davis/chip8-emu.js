589bcaa51b3d78fbec7d08c65a991d4245f8081e
Davis Goff <davispgoff@gmail.com>
2020-02-09 16:21:11 -0500

    Add basic computer functions

--- src/chip8/computer.js
+++ src/chip8/computer.js
@@ -1,6 +1,37 @@
-class Computer {
-	constructor() {
+import Chip8 from `./chip8`;
+import Video from `./video`;
 
+class Computer {
+	constructor(videoSource, audioSource) {
+		const video = new Video(videoSource);
+		const audio = audioSource ? new Audio(audioSource):null;
+
+		this.chip = new Chip8(video, audio);
+		this.clockRate = 300;
+		this.stepID = null;
+		this.timerID = null;
+	}
+
+	start() {
+
+	}
+
+	stop() {
+		if (this.stepID) {
+			window.clearInterval(this.stepID);
+		}
+
+		if (this.timerID) {
+			window.clearInterval(this.timerID);
+		}
+
+		this.stepID = null;
+		this.timerID = null;
+	}
+
+	reset() {
+		this.stop();
+		this.chip.reset();
 	}
 }
 
